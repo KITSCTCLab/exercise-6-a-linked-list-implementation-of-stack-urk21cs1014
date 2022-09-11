645b7950e64cd68f580488c724f8540cc71e0eb5
urk21cs1014 <110385851+urk21cs1014@users.noreply.github.com>
2022-09-11 21:22:44 +0530

    done

--- Main.py
+++ Main.py
@@ -10,15 +10,30 @@ class Stack:
 
   def push(self, data) -> None:
     # Write your code here
+    new_node = Node(data)
+    new_node.next = self.head
+    self.head = new_node
 
   def pop(self) -> None:
     # Write your code here
+    if(self.head!=None):
+      temp = self.head
+      self.head = temp.next
 
   def status(self):
     """
     It prints all the elements of stack.
     """
-    # Write your code here  
+    # Write your code here 
+    if(self.head  != None):
+      temp = self.head
+      while temp.next!= None:
+        print(temp.data, end = "")
+        print("=>", end = "")
+        temp = temp.next
+      print(temp.data, end = "=>")
+      
+    print("None")
 
 
 # Do not change the following code
